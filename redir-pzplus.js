0305b2684499da404b0abae1cfbfe18aa5e921e1
Pioooooo <pioooooo.sun@gmail.com>
2026-08-25 00:46:19 +0800

    Update redir-pzplus.js

--- redir-pzplus.js
+++ redir-pzplus.js
@@ -1,13 +1,19 @@
 // ==UserScript==
 // @name         Redirect to pzplus
-// @namespace    https://pzplus.tck.mn/
-// @version      1.0
 // @description  Redirect puzz.link to pzplus
+// @version      0.0.1
 // @author       Pioooooo
+// @homepageURL  https://github.com/Pioooooo/userscripts
+// @supportURL   https://github.com/Pioooooo/userscripts/issues
 // @match        *://puzz.link/p*
-// @match        *://pzprxs.vercel.app/p*
+// @match        *://pzprxs.vercel.app/p*/*
+// @match        *://pzv.jp/p*/*
 // @run-at       document-start
 // @grant        none
+// @license      MIT
+// @namespace    https://github.com/Pioooooo/userscripts
+// @icon         https://www.google.com/s2/favicons?domain=puzz.link
+// @tag          pzprjs
 // ==/UserScript==
 
 (function () {
